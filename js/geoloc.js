168596445d60f70e900e0db6c21a0f673e6cb58a
Valdrina <hoxhavaldrina@gmail.com>
2021-01-03 14:46:06 +0100

    fixes

--- js/geoloc.js
+++ js/geoloc.js
@@ -1,28 +1,41 @@
 var locationx, infoWindow;
+
+
       function initMap() {
+
         locationx = new google.maps.Map(document.getElementById('locationx'), {
           center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
+          zoom: 8
+        }
+        );
         infoWindow = new google.maps.InfoWindow;
 
 
         if (navigator.geolocation) {
+
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
+
               lat: position.coords.latitude,
               lng: position.coords.longitude
+
             };
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+
+            infoWindow.setContent('Your location is found.');
+
             infoWindow.open(locationx);
+
             locationx.setCenter(pos);
+            
           }, function() {
             handleLocationError(true, infoWindow, locationx.getCenter());
           });
         } else {
-          // IF Browser doesn't support Geolocation
+
+          // If Browser doesn't support Geolocation Api then
+
           handleLocationError(false, infoWindow, locationx.getCenter());
         }
       }
